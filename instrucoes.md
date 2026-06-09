d791adbe72e4e86d23214545493c84841b5649fe
m7zz <whitexleaf999@gmail.com>
2026-06-09 17:21:24 -0300

    commit

---
+++ instrucoes.md
@@ -0,0 +1,7 @@
+
+Pegar as atividades realizados em python:
+- Menu Supermercado
+- Par ou Impar
+- Indice de IMC
+
+Desafio: Juntar as três atividades e desenvolver um programa.
---
+++ main.py
@@ -0,0 +1,120 @@
+#FUNCAO SUPERMERCADO
+def supermercado():
+    carrinho = []
+    total = 0
+
+    while True: #MENU MERCADO
+        print("\n" + "=" * 30)
+        print("      MERCADO")
+        print("=" * 30)
+        print("1 - Adicionar produto")
+        print("2 - Ver carrinho")
+        print("3 - Ver total da compra")
+        print("4 - Finalizar compra")
+        print("0 - Voltar")
+        print("=" * 30)
+
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            nome = input("Nome do produto: ")
+            preco = float(input("Preço: R$ "))
+
+            carrinho.append((nome, preco))
+            total += preco
+
+            print(f"{nome} adicionado ao carrinho!")
+
+        elif opcao == "2":
+            if len(carrinho) == 0:
+                print("Carrinho vazio!")
+            else:
+                print("\n=== CARRINHO ===")
+                for produto, preco in carrinho:
+                    print(f"{produto} - R$ {preco:.2f}")
+
+        elif opcao == "3":
+            print(f"Total atual: R$ {total:.2f}")
+
+        elif opcao == "4":
+            print("\n=== COMPRA FINALIZADA ===")
+            print(f"Valor total: R$ {total:.2f}")
+            break
+
+        elif opcao == "0":
+            break
+
+        else:
+            print("Opção inválida!")
+
+#FUNCAO PAR OU IMPAR
+def par_ou_impar():
+    print("=" * 30)
+    print('Seja bem-vindo(a) ao sistema de verificacao de impar ou par!')
+    numero = int(input('Digite um numero:'))
+
+    if numero % 2 == 0:
+        print('Número é PAR.')
+    else:
+        print('Número é IMPAR.')
+
+#FUNCAO CALCULAR IMC
+def imc():
+    peso = float(input('Digite seu peso: '))
+    altura = float(input('Digite sua altura: '))
+
+    imc = peso / (altura ** 2 )
+
+    print(f'IMC: {imc:.2f}')
+
+    if imc < 16:
+        print("Seu estado é de Magreza grave\nProcure um médico ou nutricionista.")
+
+    elif imc < 17:
+        print("Seu estado é de Magreza moderada\nPrecisa rever sua alimentação.")
+
+    elif imc < 18.5:
+        print("Seu estado é de Magreza leve\nPrecisa rever sua alimentação.")
+
+    elif imc < 25:
+        print("Você está Saudável.\nParabéns!")
+
+    elif imc < 30:
+        print("Seu estado é de Sobrepeso\nPrecisa rever sua alimentação.")
+
+    elif imc < 35:
+        print("Seu estado é de Obesidade Grau I\nProcure um médico ou nutricionista.")
+
+    elif imc < 40:
+        print("Seu estado é de Obesidade Grau II (severa)\nProcure um médico ou nutricionista.")
+
+    else:
+        print("Seu estado é de Obesidade Grau III (mórbida)\nProcure um médico ou nutricionista.")
+
+#MENU PRINCIPAL
+while True:
+
+    print("\n=== SISTEMA MULTIFUNCIONAL ===")
+    print("1. Sistema SuperMercado")
+    print("2. Sistema Par ou Impar")
+    print("3. Sistema De IMC")
+    print("0. Sair")
+
+    opcao = int(input("Escolha uma opção: "))
+
+    if opcao == 1:
+        supermercado()
+    
+    elif opcao == 2:
+        par_ou_impar()
+
+    elif opcao == 3:
+        imc()
+
+    elif opcao == 0:
+        print("Saindo...")
+        break
+
+    else:
+        print("Opção Invalida!")
+
